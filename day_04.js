486501c93f19fc8de5135058ce0e74667390eeaf
natalie-poulson <natalieplsn@gmail.com>
2018-11-16 14:29:44 -0800

    day 4

---
+++ day_04.js
@@ -0,0 +1,23 @@
+function Person(initialAge){
+
+    if (initialAge < 0) {
+        age = 0
+        console.log("Age is not valid, setting age to 0.")
+    } else {
+        age = initialAge
+    }
+    
+    this.amIOld=function(){
+        if (age < 13) {
+        console.log("You are young.")
+        } else if (age >= 13 && age < 18) {
+            console.log("You are a teenager.")
+        } else {
+            console.log("You are old.")
+        }
+    };
+
+    this.yearPasses=function(){
+        age += 1
+    };
+}
