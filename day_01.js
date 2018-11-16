07518ec80da3c3387ea21a7214e39b26c0e16203
natalie-poulson <natalieplsn@gmail.com>
2018-11-16 14:26:57 -0800

    day 2

---
+++ day_01.js
@@ -0,0 +1,14 @@
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    
+    let ii = parseInt(readLine());
+    let dd = parseFloat(readLine());
+    let ss = readLine();
+
+    console.log(i + ii);
+    console.log((parseFloat(d) + dd).toFixed(1))
+    console.log(s + ss); 
+
+}
