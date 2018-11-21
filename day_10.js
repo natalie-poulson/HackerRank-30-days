5cacc3cbede4e0646a77f0f436c5c81c835e5fa6
natalie-poulson <natalieplsn@gmail.com>
2018-11-20 17:32:33 -0800

    day 10

---
+++ day_10.js
@@ -0,0 +1,13 @@
+function main() {
+    const n = parseInt(readLine(), 10);
+    const binary = n.toString(2)
+    const onesArray = binary.split('0')
+    let max = 0
+
+    for (var i = 0; i < onesArray.length; i++){
+        if (onesArray[i].length > max) {
+            max = onesArray[i].length
+        } 
+    }
+    return max
+}
